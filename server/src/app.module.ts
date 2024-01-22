a06468ccb0fe00cc7001cef0781343371fb51dde
frede122 <fredemanduoliveira@hotmail.com>
2024-01-22 15:28:39 -0300

    feat(server): adiciona environmnents

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -15,19 +16,19 @@ const ROUTER = [
 
 const ENTITY = [
   Category,
-
   Product
 ]
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'teste-dbf',
+      host: process.env.HOST,
+      port: Number(process.env.PORT),
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       entities: ENTITY,
       synchronize: false,
     }),
